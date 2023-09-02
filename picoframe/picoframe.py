c6e80073194319c41ba297af7bd07acdbdd00b9e
Alexander Wilde <alexander.wilde87@gmail.com>
2023-09-02 23:55:24 +0100

    Make code synchronous to be more efficient on battery

--- picoframe/picoframe.py
+++ picoframe/picoframe.py
@@ -1,7 +1,6 @@
 import inky_frame
 import jpegdec
 import mrequests as requests
-import uasyncio as asyncio
 import uos as os
 from picographics import DISPLAY_INKY_FRAME_7 as DISPLAY
 from picographics import PicoGraphics
@@ -15,7 +14,6 @@ class PicoFrame:
     self.graphics = PicoGraphics(DISPLAY)
     self.jpg = jpegdec.JPEG(self.graphics)
 
-    self.image_lock = asyncio.Lock()
     self.image_url = config.IMAGE_URL.rstrip('/')
     self.image_dir = config.IMAGE_DIR.rstrip('/')
     self.image = f'{self.image_dir}/image.jpg'
@@ -26,34 +24,35 @@ class PicoFrame:
     except:
       pass
 
-  async def display_image(self):
+  def display_image(self):
     print('display_image')
-    async with self.image_lock:
+    try:
       self.jpg.open_file(self.image)
+      self.jpg.decode(0, 0, jpegdec.JPEG_SCALE_FULL)
+      self.graphics.update()
+    except Exception as e:
+      print(e)
 
-    self.jpg.decode(0, 0, jpegdec.JPEG_SCALE_FULL)
-    self.graphics.update()
-
-  async def download_random_image(self):
+  def download_random_image(self):
     print('download_random_image')
     r = requests.get(self.image_url, headers={
       'Authorization': f'Basic {config.API_KEY}',
     })
     if r.status_code != 200:
+      self.display_image()
       return
 
-    async with self.image_lock:
-      self.mkdir()
-      r.save(self.image)
+    self.mkdir()
+    r.save(self.image)
 
-    await self.display_image()
+    self.display_image()
 
 
-async def async_loop(coro, delay: int):
+def loop(func, minutes: int):
   while True:
     with picoframe.led_busy():
-      await asyncio.create_task(coro())
-    await asyncio.sleep(delay)
+      func()
+    inky_frame.sleep_for(minutes)
 
 
 def app_mode():
@@ -62,6 +61,4 @@ def app_mode():
       inky_frame.set_time()
 
     frame = PicoFrame()
-    loop = asyncio.get_event_loop()
-    loop.create_task(async_loop(frame.download_random_image, config.IMAGE_UPDATE))
-    loop.run_forever()
+    loop(frame.download_random_image, config.IMAGE_UPDATE_INTERVAL)
